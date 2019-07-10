94c09285867c17b99d50f8b9da0d32d89f45ae55
unknown <williomos@gmail.com>
2019-07-10 13:26:50 +0300

    [ft-166742750] modifying the logo

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
+// eslint-disable-next-line linebreak-style
 import express from 'express';
 
-
 const app = express();
-
-
 app.use(express.json());
 app.use(express.urlencoded({
-    extended: false
+  extended: false,
 }));
 
 export default app;
